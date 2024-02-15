baf4c2cb11aeb5671128e5f1f9c3722a95527408
Alex Majorowicz <alexmajors0613@gmail.com>
2024-02-15 11:24:33 -0600

    troubleshoot render 6

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,13 +14,13 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
-    build: {
+    /* build: {
       // generate .vite/manifest.json in outDir
       manifest: true,
       rollupOptions: {
         // overwrite default .html entry
         input: "/path/to/main.jsx",
       },
-    },
+    }, */
   },
 });
