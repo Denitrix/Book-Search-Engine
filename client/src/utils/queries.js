89ab20c91709b6a08569e2347d90707937516f85
Alex Majorowicz <alexmajors0613@gmail.com>
2024-02-15 06:14:21 -0600

    add GET_ME query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,20 @@
 // TODO: create GET_ME query
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        title
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+`;
